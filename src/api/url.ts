c2287d5b9618f11e73410db004ef50cbe1119cb7
Chetha <vuthychetha@gmail.com>
2024-10-07 15:17:32 +0700

    ✨ Add new API endpoints for SystemUser, Category, Poster, BusinessType, Role, Banner, and RequestDesign

--- src/api/url.ts
+++ src/api/url.ts
@@ -27,46 +27,55 @@ export const BRANCH = {
   UPDATE: '/branch',
 }
 
-// export const Category = {
-//   CREATE: '/category/create',
-//   LIST: '/category',
-//   DELETE: '/category/delete',
-//   UPDATE: '/category/update',
-//   GET: '/category',
-// }
+export const SystemUser = {
+  CREATE: '/system-user/create',
+  LOGIN: '/system-user/login',
+  LIST: '/system-user/list',
+  DELETE: '/system-user/delete',
+  UPDATE: '/system-user/update',
+  GET: '/system-user',
+}
 
-// export const Poster = {
-//   CREATE: '/poster/admin/create',
-//   LIST: '/poster/admin/list',
-//   DELETE: '/poster/admin/delete',
-//   UPDATE: '/poster/admin/update',
-//   GET: '/poster/admin',
-// }
+export const Category = {
+  CREATE: '/category/create',
+  LIST: '/category',
+  DELETE: '/category/delete',
+  UPDATE: '/category/update',
+  GET: '/category',
+}
 
-// export const BusinessType = {
-//   CREATE: '/business-type/create',
-//   LIST: '/business-type/list',
-//   DELETE: '/business-type/delete',
-//   UPDATE: '/business-type/update',
-//   GET: '/business-type',
-// }
+export const Poster = {
+  CREATE: '/poster/admin/create',
+  LIST: '/poster/admin/list',
+  DELETE: '/poster/admin/delete',
+  UPDATE: '/poster/admin/update',
+  GET: '/poster/admin',
+}
 
-// export const Role = {
-//   CREATE: '/role/create',
-//   LIST: '/role/list',
-//   DELETE: '/role/delete',
-//   UPDATE: '/role/update',
-//   GET: '/role',
-// }
+export const BusinessType = {
+  CREATE: '/business-type/create',
+  LIST: '/business-type/list',
+  DELETE: '/business-type/delete',
+  UPDATE: '/business-type/update',
+  GET: '/business-type',
+}
 
-// export const Banner = {
-//   CREATE: '/banner/admin/create',
-//   LIST: '/banner/admin/list',
-// }
+export const Role = {
+  CREATE: '/role/create',
+  LIST: '/role/list',
+  DELETE: '/role/delete',
+  UPDATE: '/role/update',
+  GET: '/role',
+}
 
-// export const RequestDesign = {
-//   LIST: '/request-design/admin/list',
-// }
+export const Banner = {
+  CREATE: '/banner/admin/create',
+  LIST: '/banner/admin/list',
+}
+
+export const RequestDesign = {
+  LIST: '/request-design/admin/list',
+}
 
 export const test = '/test'
 
